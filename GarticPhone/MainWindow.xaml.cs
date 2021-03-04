1b34f6535a70b1d0ec8794c21bb1ea1fc253f880
Corentin Lizy <corentinyzil@gmail.com>
2021-03-04 20:25:27 +0100

    04.03

--- GarticPhone/MainWindow.xaml.cs
+++ GarticPhone/MainWindow.xaml.cs
@@ -31,11 +31,19 @@ namespace GarticPhone
         line,
         fill
     }
+
+    public enum RollBack
+    {
+        soft,
+        undo,
+        redo
+    }
     public partial class MainWindow : Window
     {
 		private SKPath _skPath;
 		private SKColor _skColor = new SKColor(0,0,0);
 		private SKPoint? _skPoint;
+        private SKPoint  _secondSkPoint;
         private SKPaintStyle _isFilled = SKPaintStyle.Stroke;
         private int _strokeWidth = 5;
 
@@ -46,6 +54,7 @@ namespace GarticPhone
 		private bool _isMouseDown = false;
 		
         private State _state = State.pen;
+        private RollBack _rollBack = RollBack.soft;
 
 		public Window Window => Application.Current.MainWindow;
 		public DpiScale DpiScale => VisualTreeHelper.GetDpi(Window);
@@ -58,12 +67,11 @@ namespace GarticPhone
 
 		private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
 		{
-			var info = e.Info;
 			var surface = e.Surface;
 			var canvas = surface.Canvas;
 
-            //Verification
-            if (_state == State.squareFilled || _state == State.circleFilled)
+            //Verifications
+            if (_state == State.squareFilled || _state == State.circleFilled || _state == State.fill)
             {
                 _isFilled = SKPaintStyle.Fill;
             }
@@ -119,9 +127,11 @@ namespace GarticPhone
                     break;
 
                 case State.line:
+                    canvas.DrawLine(_skPoint.Value, _secondSkPoint, paint);
                     break;
 
                 case State.fill:
+                    canvas.DrawPoint(_skPoint.Value, paint);
                     break;
             }
             
@@ -138,8 +148,6 @@ namespace GarticPhone
                     _skPoint = new SKPoint((float)x - 250, (float)y - 150);
                     skElement.InvalidateVisual();
                     break;
-                case State.fill: 
-                    break;
                 case State.square:
                     break;
                 case State.squareFilled:
@@ -147,6 +155,8 @@ namespace GarticPhone
                 case State.circle:
                     break;
                 case State.circleFilled:
+                    break;
+                case State.line:
                     break;
             }
 		}
@@ -213,6 +223,7 @@ namespace GarticPhone
                     break;
 
                 case State.line:
+                    _skPoint = new SKPoint((float)x - 250, (float)y - 150);
                     break;
 
                 case State.fill:
@@ -254,6 +265,8 @@ namespace GarticPhone
                     break;
 
                 case State.line:
+                    _secondSkPoint = new SKPoint((float)x - 250, (float)y - 150);
+                    skElement.InvalidateVisual();
                     break;
 
                 case State.fill:
@@ -289,109 +302,91 @@ namespace GarticPhone
 
         private void BlackColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(0, 0, 0);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
         }
 
         private void SlateGrayColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(112, 128, 144);
-            CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(112, 128, 0));
+            CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(112, 128, 144));
         }
 
         private void WhiteColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(255, 255, 255);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
         }
 
         private void MediumOrchidColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(186, 85, 211);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(186, 85, 211));
         }
 
         private void DarkOrchid(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(153, 50, 204);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(153, 50, 204));
         }
 
         private void DarkBlueColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(0, 0, 139);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 139));
         }
 
         private void MediumSlateBlueColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(123, 104, 238);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(123, 104, 238));
         }
 
         private void LightSkyBlueColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(135, 206, 250);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(135, 206, 250));
         }
 
         private void YellowGreenColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(154, 205, 50);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(154, 205, 50));
         }
 
         private void GreenColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(0, 128, 0);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(0, 128, 0));
         }
 
         private void YellowColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(255, 255, 0);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(255, 255, 0));
         }
 
         private void OrangeColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(255, 165, 0);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(255, 165, 0));
         }
 
         private void PinkColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(255, 192, 203);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(255, 192, 203));
         }
 
         private void HotPinkColor(object sender, RoutedEventArgs e)
         {
-			_skColor = new SKColor(255, 105, 180);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(255, 105, 180));
         }
 
         private void RedColor(object sender, RoutedEventArgs e)
         {
-            _skColor = new SKColor(255, 0, 0);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
         }
 
         private void FireBrickColor(object sender, RoutedEventArgs e)
         {
-            _skColor = new SKColor(178, 34, 34);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(178, 34, 34));
         }
 
         private void AnthiqueWhiteColor(object sender, RoutedEventArgs e)
         {
-            _skColor = new SKColor(250, 235, 215);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(250, 235, 215));
         }
 
         private void SaddleBrownColor(object sender, RoutedEventArgs e)
         {
-            _skColor = new SKColor(139, 69, 19);
             CurrentColor.Fill = new SolidColorBrush(Color.FromRgb(139, 69, 19));
         }
 
@@ -403,7 +398,6 @@ namespace GarticPhone
         private void OnRubber(object sender, RoutedEventArgs e)
         {
             _state = State.rubber;
-            _skColor = new SKColor(255, 255, 255);
         }
 
         private void OnSquare(object sender, RoutedEventArgs e)
@@ -429,22 +423,23 @@ namespace GarticPhone
 
         private void OnLine(object sender, RoutedEventArgs e)
         {
-
+            _state = State.line;
         }
 
         private void OnFill(object sender, RoutedEventArgs e)
         {
-
+            _state = State.fill;
         }
 
         private void OnCancel(object sender, RoutedEventArgs e)
         {
-
+            _rollBack = RollBack.undo;
+            skElement.InvalidateVisual();
         }
 
         private void OnRestore(object sender, RoutedEventArgs e)
         {
-
+            _rollBack = RollBack.redo;
         }
     }
 }
